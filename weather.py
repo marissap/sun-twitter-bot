a9f1e10e0c70af7c5c378daf3f181f6c71816884
Marissa Phul <mphul044@uottawa.ca>
2018-10-31 00:02:00 -0400

    formatted time

--- weather.py
+++ weather.py
@@ -1,6 +1,8 @@
 import json
 import tweepy
 import requests
+
+import time
 import datetime
 
 response = requests.get('https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_astronomy&name=Ottawa&app_id=t883KznRyE8cWLnJaYkn&app_code=MnUR3KfQTWo1MDxu92e1BA')
@@ -16,12 +18,13 @@ sunStuff.append(data["astronomy"]["astronomy"][1]["sunrise"])
 sunStuff.append(data["astronomy"]["astronomy"][1]["sunset"])
 
 count = 0
+ts = time.time()
 st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 
 for change in sunStuff:
     if count == 0:
-        change = st + "Hello Today! Sunrise: " + change
+        change = st + "\nHello Today! Sunrise: " + change
     else:
-        change = st + "Goodbye Today! Sunset: " + change
+        change = st + "\nGoodbye Today! Sunset: " + change
     count += 1
     api.update_status(change)
